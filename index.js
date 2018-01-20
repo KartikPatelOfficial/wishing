ce99cb68e1e61dae8c49187604adc5f61bac605c
Kartik Patel <patelkartik1910@gmail.com>
2018-01-21 03:24:21 +0530

    added watsapp

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ var app = express()
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
+
+
 app.get('/name/:name', function (req, res) {
     var name = req.params.name
     res.render('main', { name: name })
 })
-
 
 app.listen(8080, function () {
     console.log(__dirname)
