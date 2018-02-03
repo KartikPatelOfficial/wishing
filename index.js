bdd2d262b3fcbb8d9d1ee7d413d11bcdf4ab9341
Kartik Patel <patelkartik1910@gmail.com>
2018-02-03 20:04:49 +0530

    final V2.0

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 var express = require('express')
 var app = express()
 
+var ob = {
+    type : "Republic Day",
+    quote : "Our life is full of colors.I hope this 26th january will add more colors to your life.Happy Republic Day!"
+}
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
+app.get('/:type/:name', function (req, res) {
 
-
-app.get('/republic/:name', function (req, res) {
+    var newType = checkType(req.params.type) 
     var name = req.params.name
-    res.render('republic', { name: name })
+
+    res.render('republic', { name: name ,type: newType.type ,quote : newType.quote })
 })
 
 app.get('/', function (req, res) {
@@ -19,3 +25,39 @@ app.get('/', function (req, res) {
 app.listen(8080, function () {
     console.log(__dirname)
 })
+
+function checkType(tp){
+    var returnable;
+
+    switch(tp){
+        case "0":{
+            returnable = ob;
+        }break;
+
+        case "1":{
+            ob.quote = "saf"
+            ob.type = "Mahasivratri"
+            returnable = ob;
+        }break;
+
+        case "2":{
+            ob.quote = "saf"
+            ob.type = "Mahasivratri"
+            returnable = ob;        
+        }break;
+
+        case "3":{
+            ob.quote = "saf"
+            ob.type = "Mahasivratri"
+            returnable = ob;        
+        }break;
+
+        default:{
+            returnable = ob;
+        }
+
+    }
+
+    return returnable
+
+}
